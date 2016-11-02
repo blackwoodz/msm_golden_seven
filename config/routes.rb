738614212edaa5f3c42dc5c79569b5a6880df7b1
Nish George <skarlath001@gmail.com>
2016-11-02 18:04:43 -0500

    Movies RCAV complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
-  
+
+  # DEFAULT
+  get("/", {controller: "movies", action: "index"})
+
+  # MOVIES : delete
+  get("/delete_movie/:id", {controller: "movies", action: "destroy"})
+
+  # MOVIES : create
+  get("/movies/new_form", {controller: "movies", action: "new_form"})
+  get("/movies/create_row", {controller: "movies", action: "create_row"})
+
+  # MOVIES : update
+  get("/movies/:id/edit_form", {controller: "movies", action: "edit_form"})
+  get("/movies/update_movie/:id", {controller: "movies", action: "update_row"})
+
+  # MOVIES : read
+  get("/movies", {controller: "movies", action: "index"})
+  get("/movies/:id", {controller: "movies", action: "show"})
+
 end
